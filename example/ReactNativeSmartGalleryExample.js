12d9b80308bcba9f54c4cd677db734f1624c45b6
Lue Hang <luehang17@gmail.com>
2019-12-27 21:24:40 -0600

    Update example to React Native 0.61.

--- example/ReactNativeSmartGalleryExample.js
+++ example/ReactNativeSmartGalleryExample.js
@@ -4,16 +4,18 @@ import {
     View
 } from "react-native";
 import SmartGallery from "react-native-smart-gallery";
+// import SmartGallery from "./src";
 
 import testData from "./data";
 
 export default class ReactNativeSmartGalleryExample extends React.PureComponent {
     render() {
-        console.log("Data length total: ", testData.length);
+        // console.log("Data length total: ", testData.length);
         return (
             <View
                 style={styles.container}>
                 <SmartGallery
+                    // index={2}
                     style={{ flex: 1, backgroundColor: "#000" }}
                     // sensitiveScroll={true}
                     images={testData}
--- example/data.js
+++ example/data.js
@@ -1,5 +1,5 @@
 function idGenerator() {
-    return Math.random().toString(36).substr(2, 9);
+    return Math.random().toString(36).substring(7);
 }
 
 const data = [
