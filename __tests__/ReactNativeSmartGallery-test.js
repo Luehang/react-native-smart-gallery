e3b6d87fa2b7c3ad616b5e67e9a85ab3b48c5dab
Kaidoj <jast112@gmail.com>
2020-03-22 14:42:29 +0200

    Added possibility to add toolbar for image slide views

--- __tests__/ReactNativeSmartGallery-test.js
+++ __tests__/ReactNativeSmartGallery-test.js
@@ -1,13 +1,41 @@
-import "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import React from "react";
 import {
     data
 } from "./mocks/dataMock";
-import SmartGallery from "../src";
+import SmartGallery from "../src/index";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 
-it("React Native Smart Gallery renders correctly", () => {
-    renderer.create(<SmartGallery images={data} />);
+describe("React Native Smart Gallery", () => {
+    it("should render correctly", () => {
+        const r = renderer.create(<SmartGallery images={data} />);
+        expect(JSON.stringify(r.toJSON())).not.toMatch(new RegExp("Toolbar button"));
+    });
+
+    it("should render toolbar correctly", () => {
+        const r = renderer.create(<SmartGallery
+            images={data} 
+            toolbarItem={() => (
+                <View>
+                    <TouchableOpacity
+                        onPress={() => jest.fn()}
+                    >
+                        <Text>Toolbar button</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        />);
+        expect(JSON.stringify(r.toJSON())).toMatch(new RegExp("Toolbar button"));
+    });
+
+    it("should consume image container styles", () => {
+        const r = renderer.create(<SmartGallery
+            images={data}
+            imageContainerStyle={{ backgroundColor: "#f1f2f3" }}
+        />);
+        expect(r.toJSON())
+            .toHaveProperty("props.imageContainerStyle.backgroundColor", "#f1f2f3");
+    });
 });
